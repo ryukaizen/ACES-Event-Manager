97f67676d108edf2048c4cb2be6e8119b123bd8e
Ryukaizen <ryukaizen@protonmail.com>
2022-12-28 13:55:54 +0530

    add events to the db, then display on treeview

--- src/dashboard/sections/events.py
+++ src/dashboard/sections/events.py
@@ -1,8 +1,9 @@
 import customtkinter
 import mysql.connector
 import time
+import uuid
 
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from tkcalendar import Calendar
 from datetime import datetime, timedelta
 from ...database.db_connect import cursor, cnx
@@ -112,32 +113,33 @@ class EventSection:
         style.map("Treeview.Heading", background=[('active', '#0065D9')])
         
         treeview = ttk.Treeview(frame)
-        treeview["columns"] = ("sr", "event_id", "event_name", "event_description", "event_date", "event_time", "event_venue")
+        treeview["columns"] = ("sr", "event_name", "event_description", "event_date", "event_time", "event_venue", "event_id",)
         treeview["show"] = "headings"
         
         treeview.column("sr", width=50, anchor="center")
-        treeview.column("event_id", width=250, anchor="center")
         treeview.column("event_name", width=200, anchor="center")
         treeview.column("event_description", width=300, anchor="center")
         treeview.column("event_date", width=80, anchor="center")
         treeview.column("event_time", width=80, anchor="center")
         treeview.column("event_venue", width=100, anchor="center")
+        treeview.column("event_id", width=250, anchor="center")
         
         treeview.heading("sr", text="Sr. No")
-        treeview.heading("event_id", text="Event ID")
         treeview.heading("event_name", text="Title")
         treeview.heading("event_description", text="Description")
         treeview.heading("event_date", text="Date")
         treeview.heading("event_time", text="Time")
         treeview.heading("event_venue", text="Location")
+        treeview.heading("event_id", text="Event ID")
 
         treeview.grid(row=1, column=2, rowspan=5, sticky="nsew", padx=10, pady=10)
         
-        query = "SELECT event_id, event_name, event_description, event_date, event_time, event_venue FROM events"
+        query = "SELECT event_name, event_description, event_date, event_time, event_venue, event_id FROM events"
         while True:
             try:
                 cursor.execute(query)
                 results = cursor.fetchall()
+                print(results)
                 break
             except mysql.connector.Error as err:
                 print(f"[x] Error occured while executing query: {query}\n{err}")
@@ -149,12 +151,13 @@ class EventSection:
                 i=0
                 for row in results:
                     i=i+1
-                    treeview.insert("", 0, text="", values=(i, row))
+                    treeview.insert("", "end", text="", values=(i, row[0], row[1], row[2], row[3], row[4], row[5]))
 
 class NewEvent:
     def __init__(self, frame):
         for widget in frame.winfo_children():
             widget.destroy()
+        self.the_frame = frame
         self.new_event(frame)
     
     def new_event(self, frame):
@@ -318,7 +321,7 @@ class NewEvent:
                         border_color=("#1B1B24", "#EDF6FA"),
                         corner_radius=10,
                         )
-        self.event_venue_entry.place(x=1040, y=300, anchor="center")
+        self.event_venue_entry.place(x=1050, y=300, anchor="center")
         
         submit_event_button = customtkinter.CTkButton(
                         frame,
@@ -331,7 +334,7 @@ class NewEvent:
                         border_width=3,
                         border_color=("#EDF6FA", "#1B1B24"),
                         corner_radius=15,
-                        command=self.add_event
+                        command=self.check_event_details
                         )
         submit_event_button.place(x=1040, y=400, anchor="center")    
     
@@ -345,12 +348,124 @@ class NewEvent:
         self.selected_time_label.configure(text = "Selected Time: " + self.selected_time)
         return self.selected_time
     
-    def add_event(self):
+    def check_event_details(self):
         self.event_name = self.event_name_entry.get()
         self.event_description = self.event_description_entry.get("0.0", "end")
         self.event_date = self.select_date()
         self.event_time = self.select_time()
         self.event_venue = self.event_venue_entry.get("0.0", "end")
         
-        print(self.event_name, self.event_description, self.event_date, self.event_time,  self.event_venue)
+        error_message = ""
+        if not self.event_name:
+            error_message += "Please provide event name.\n"
+        if len(self.event_description) == 1:
+            error_message += "Please provide event description.\n"
+        if len(self.event_venue) == 1:
+            error_message += "Please provide event venue.\n"
+              
+        if error_message:
+            messagebox.showerror("Error", error_message)
+        else:
+            self.dialogue_window = customtkinter.CTkToplevel(fg_color=("#EDF6FA", "#1B1B24"))
+            self.dialogue_window.geometry("500x400")
+            self.dialogue_window.title("Proceed to add event?")
+            self.dialogue_window.resizable(False, False)
+            
+            question_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Are you sure this information is correct?",
+                            font=customtkinter.CTkFont(size=16),
+                            )
+            question_label.place(x=250, y=20, anchor="center")
+            
+            event_name_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Event Name: " + self.event_name,
+                            font=customtkinter.CTkFont(size=16),
+                            )
+            event_name_label.place(x=250, y=60, anchor="center")
+            
+            event_description_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Event Description: " + f"{self.event_description[:15]}...",
+                            font=customtkinter.CTkFont(size=16),
+                            )
+            event_description_label.place(x=250, y=100, anchor="center")
+            
+            event_date_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Event Date: " + self.event_date,
+                            font=customtkinter.CTkFont(size=16),
+                            )
+            event_date_label.place(x=250, y=140, anchor="center")
+            
+            event_time_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Event Time: " + self.event_time,
+                            font=customtkinter.CTkFont(size=16),
+                            )
+            event_time_label.place(x=250, y=180, anchor="center")
+            
+            event_venue_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Event Venue: " + self.event_venue,
+                            font=customtkinter.CTkFont(size=16),
+                            )
+            event_venue_label.place(x=250, y=220, anchor="center")
+            
+            yes_button = customtkinter.CTkButton(
+                            self.dialogue_window,
+                            text="Yes (Add Event)",
+                            width=140,
+                            height=60,
+                            font=customtkinter.CTkFont(size=16),
+                            fg_color="#0065D9",
+                            border_width=2,
+                            border_color=("#1B1B24","#EDF6FA"),
+                            corner_radius=15,
+                            command=self.add_event_to_database
+                            )
+            yes_button.place(x=145, y=330, anchor="center")
+            
+            no_button = customtkinter.CTkButton(
+                            self.dialogue_window,
+                            text="No (Edit Again)",
+                            width=140,
+                            height=60,
+                            font=customtkinter.CTkFont(size=16),
+                            fg_color="#0065D9",
+                            border_width=2,
+                            border_color=("#1B1B24","#EDF6FA"),
+                            corner_radius=15,
+                            command=self.dialogue_window.destroy
+                            )
+            no_button.place(x=370, y=330, anchor="center")
+            
+    def add_event_to_database(self):
         
+        # This generates unique ID for event 
+        self.event_id = uuid.uuid5(uuid.NAMESPACE_OID, f"{self.event_name}{self.event_date}{self.event_time}{self.event_venue}")
+        
+        # Escape special characters in event description
+        eventdesc = mysql.connector.conversion.MySQLConverter().escape(self.event_description)
+        
+        # For future purposes (date and time is being stored as string as of now)
+        # # This converts the date from string to datetime object 
+        # date_format = "%d/%m/%y"  
+        # date = datetime.strptime(self.event_date, date_format)
+        
+        # # This converts the time from string to datetime object     
+        # time_format = "%I:%M %p"
+        # time = datetime.strptime(self.event_time, time_format)
+        
+        try:
+            cursor.execute("""INSERT INTO events(event_name, event_description, event_date, event_time, event_venue, event_id) VALUES ("{}", "{}", "{}", "{}", "{}", "{}");""".format(
+                                self.event_name, eventdesc, self.event_date, self.event_time,  self.event_venue, self.event_id)
+                        )
+            cnx.commit()                         
+        except Exception as error:
+            raise Exception("Error", str(error))  
+        else:
+            self.dialogue_window.destroy()
+            EventSection(self.the_frame)
+            

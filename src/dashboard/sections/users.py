18537f76f94a2964600dc6f036592b077953a266
Ryukaizen <ryukaizen@protonmail.com>
2023-01-01 19:17:42 +0530

    update user details by admin

--- src/dashboard/sections/users.py
+++ src/dashboard/sections/users.py
@@ -40,7 +40,7 @@ class UsersSection:
                         border_width=3,
                         border_color=("#EDF6FA", "#1B1B24"),
                         corner_radius=15,
-                        # command= lambda: MarkCompleted(frame, treeview)
+                        command= lambda: UpdateDetails(frame, treeview)
                         )
         update_details_button.grid(row=2, column=1, sticky="n", padx=35, pady=5)
         
@@ -416,7 +416,7 @@ class RegisterUser:
                 try:
                     cursor.execute("SELECT * FROM users WHERE username = {};".format(self.username)) 
                     if cursor.fetchone() is not None:
-                        messagebox.showerror("Error", "PRN already exists in the database.\nKindly login instead.")
+                        messagebox.showerror("Error", "PRN already exists in the database.\nKindly update it at Update Details section.")
                         return False
                     else:
                         cursor.execute("""INSERT INTO users(username, password, full_name, email, mobile, year, gender, role) VALUES ({}, "{}", "{}", "{}", {}, "{}", "{}", "{}");""".format(
@@ -437,3 +437,315 @@ class RegisterUser:
                         messagebox.showinfo("Registration Successful", f"Registration Successful!\n\nUsername added: {self.username}")
                         print("Registration Successful. Username added: " + self.username)
                         UsersSection(self.the_frame)
+
+class UpdateDetails:
+    def __init__(self, frame, treeview):
+        self.the_frame = frame
+        try:
+            selected_item = treeview.selection()[0]
+            self.username = treeview.item(selected_item)['values'][1]
+            self.name = treeview.item(selected_item)['values'][2]
+            self.email = treeview.item(selected_item)['values'][3]
+            self.mobile = treeview.item(selected_item)['values'][4]
+            self.year = treeview.item(selected_item)['values'][5]
+            self.gender = treeview.item(selected_item)['values'][6]
+            self.role = treeview.item(selected_item)['values'][7]
+               
+        except IndexError:
+            messagebox.showerror("Error", "Please select a user to proceed.")
+        else:
+            for widget in frame.winfo_children():
+                widget.destroy()
+            self.update_user(frame)              
+    
+    def update_user(self, frame):
+        
+        go_back_button = customtkinter.CTkButton(
+                        frame,
+                        text="Go Back",
+                        font=customtkinter.CTkFont(size=16),
+                        fg_color="Green",
+                        hover_color="#2AAAFA",
+                        border_width=3,
+                        border_color=("#EDF6FA", "#1B1B24"),
+                        corner_radius=15,
+                        command=lambda: UsersSection(frame)
+                        )
+        go_back_button.place(relx=0.1, rely=0.02, anchor="n") 
+        
+        update_user_label = customtkinter.CTkLabel(
+                        frame,
+                        text='Update User Details',
+                        font=customtkinter.CTkFont(size=26, weight="bold"),
+                        )
+        update_user_label.place(relx=0.5, rely=0.02, anchor="n")  
+        
+        register_username_label = customtkinter.CTkLabel(
+                        frame,
+                        text="Username",
+                        text_color="Grey",
+                        font=customtkinter.CTkFont(size=20),
+                        width=50,
+                        height=25,
+                        corner_radius=0
+                        )
+        register_username_label.place(relx=0.1, rely=0.15, anchor="n")
+
+        self.register_username_entry = customtkinter.CTkEntry(
+                        frame,
+                        width=290,
+                        height=35,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=4,
+                        border_color="Grey",
+                        corner_radius=10,
+                        state="disabled"
+                        )
+        self.register_username_entry.place(relx=0.17, rely=0.21, anchor="n")
+
+        register_password_label = customtkinter.CTkLabel(
+                        frame,
+                        text="Password",
+                        font=customtkinter.CTkFont(size=20),
+                        width=50,
+                        height=25,
+                        corner_radius=0
+                        )
+        register_password_label.place(relx=0.1, rely=0.35, anchor="n")
+    
+        self.register_password_entry = customtkinter.CTkEntry(
+                        frame,
+                        placeholder_text="Password",
+                        show="*",
+                        width=290,
+                        height=35,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=3,
+                        border_color=("#1B1B24", "#EDF6FA"),
+                        corner_radius=10
+                        )
+        self.register_password_entry.place(relx=0.17, rely=0.41, anchor="n")
+        
+        full_name_label = customtkinter.CTkLabel(
+                        frame,
+                        text="Full Name",
+                        font=customtkinter.CTkFont(size=20),
+                        width=50,
+                        height=25,
+                        corner_radius=0
+                        )
+        full_name_label.place(relx=0.1, rely=0.55, anchor="n")
+
+        self.full_name_entry = customtkinter.CTkEntry(
+                        frame,
+                        placeholder_text="Full name",
+                        width=290,
+                        height=35,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=3,
+                        border_color=("#1B1B24", "#EDF6FA"),
+                        corner_radius=10
+                        )
+        self.full_name_entry.place(relx=0.17, rely=0.61, anchor="n")
+        
+        email_label = customtkinter.CTkLabel(
+                        frame,
+                        text="Email ID",
+                        font=customtkinter.CTkFont(size=20),
+                        width=50,
+                        height=25,
+                        corner_radius=0
+                        )
+        email_label.place(relx=0.4, rely=0.15, anchor="n")
+
+        self.email_entry = customtkinter.CTkEntry(
+                        frame,
+                        placeholder_text="Email ID",
+                        width=290,
+                        height=35,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=3,
+                        border_color=("#1B1B24", "#EDF6FA"),
+                        corner_radius=10
+                        )
+        self.email_entry.place(relx=0.48, rely=0.21, anchor="n")
+        
+        mobile_label = customtkinter.CTkLabel(
+                        frame,
+                        text="Mobile No.",
+                        font=customtkinter.CTkFont(size=20),
+                        width=50,
+                        height=25,
+                        corner_radius=0
+                        )
+        mobile_label.place(relx=0.41, rely=0.35, anchor="n")
+
+        self.mobile_entry = customtkinter.CTkEntry(
+                        frame,
+                        placeholder_text="Mobile No.",
+                        width=290,
+                        height=35,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=3,
+                        border_color=("#1B1B24", "#EDF6FA"),
+                        corner_radius=10
+                        )
+        self.mobile_entry.place(relx=0.48, rely=0.41, anchor="n")
+        
+        year_label = customtkinter.CTkLabel(
+                        frame,
+                        text="Year",
+                        font=customtkinter.CTkFont(size=20),
+                        width=50,
+                        height=25,
+                        corner_radius=0
+                        )
+        year_label.place(relx=0.39, rely=0.55, anchor="n")
+        
+        self.year_optionmenu = customtkinter.CTkOptionMenu(
+                                frame, 
+                                width=200,
+                                height=40,
+                                font=customtkinter.CTkFont(size=18),
+                                dropdown_font=customtkinter.CTkFont(size=18),
+                                values=["First Year", "Second Year", "Third Year", "Final Year"],
+                                corner_radius=10
+                                )
+        self.year_optionmenu.place(relx=0.45, rely=0.61, anchor="n")
+        
+        gender_label = customtkinter.CTkLabel(
+                        frame,
+                        text="Gender",
+                        font=customtkinter.CTkFont(size=20),
+                        width=50,
+                        height=25,
+                        corner_radius=0
+                        )
+        gender_label.place(relx=0.7, rely=0.15, anchor="n")
+        
+        self.gender_radio_var = customtkinter.StringVar(value="Male")
+        
+        self.gender_radio_button_1 = customtkinter.CTkRadioButton(
+                                frame, 
+                                text="Male", 
+                                font=customtkinter.CTkFont(size=18),
+                                variable=self.gender_radio_var, 
+                                value="Male"
+                                )
+        self.gender_radio_button_1.place(relx=0.72, rely=0.21, anchor="n")
+        
+        self.gender_radio_button_2 = customtkinter.CTkRadioButton(
+                                frame, 
+                                text="Female", 
+                                font=customtkinter.CTkFont(size=18),
+                                variable=self.gender_radio_var, 
+                                value="Female"
+                                )
+        self.gender_radio_button_2.place(relx=0.79, rely=0.21, anchor="n")
+        
+        role_label = customtkinter.CTkLabel(
+                        frame,
+                        text="Role",
+                        font=customtkinter.CTkFont(size=20),
+                        width=50,
+                        height=25,
+                        corner_radius=0
+                        )
+        role_label.place(relx=0.69, rely=0.35, anchor="n")
+        
+        self.role_radio_var = customtkinter.StringVar(value="USER")
+        
+        self.role_radio_button_1 = customtkinter.CTkRadioButton(
+                                frame, 
+                                text="Admin", 
+                                font=customtkinter.CTkFont(size=18),
+                                variable=self.role_radio_var, 
+                                value="ADMIN",
+                                )
+        self.role_radio_button_1.place(relx=0.72, rely=0.41, anchor="n")
+        
+        self.role_radio_button_2 = customtkinter.CTkRadioButton(
+                                frame, 
+                                text="User", 
+                                font=customtkinter.CTkFont(size=18),
+                                variable=self.role_radio_var, 
+                                value="USER"
+                                )
+        self.role_radio_button_2.place(relx=0.79, rely=0.41, anchor="n")
+        
+        update_button = customtkinter.CTkButton(
+                        frame,
+                        text="Update",
+                        font=customtkinter.CTkFont(size=20, weight="bold"), 
+                        fg_color="#D96C00",
+                        hover_color="#FF8C00",  
+                        width=180, 
+                        height=65, 
+                        border_width=3,
+                        border_color=("#EDF6FA", "#1B1B24"),
+                        corner_radius=15,
+                        command=self.check_user_details
+                        )
+        update_button.place(relx=0.74, rely=0.61, anchor="n")
+        
+        ############################### Add existing users values to the form ###############################
+        
+        cursor.execute("""SELECT password FROM users WHERE username = "{}";""".format(self.username))
+        self.password = cursor.fetchone()
+        self.register_password_entry.insert(0, self.password)
+        
+        self.full_name_entry.insert(0, self.name)
+        self.email_entry.insert(0, self.email)
+        self.mobile_entry.insert(0, self.mobile)
+        self.year_optionmenu.set(self.year)
+        self.gender_radio_var.set(self.gender)
+        self.role_radio_var.set(self.role)
+   
+    def check_user_details(self):
+        self.password = self.register_password_entry.get()
+        self.name = self.full_name_entry.get()
+        self.email = self.email_entry.get()
+        self.mobile = self.mobile_entry.get()
+        self.year = self.year_optionmenu.get()
+        self.gender = self.gender_radio_var.get()
+        self.role = self.role_radio_var.get()
+        
+        error_message = ""
+        if not self.password:
+            error_message += "Please enter a password.\n"
+        if not self.name:
+            error_message += "Please enter a name.\n"
+        if not self.email:
+            error_message += "Please enter an email.\n"
+        if not self.mobile:
+            error_message += "Please enter a mobile number.\n"
+        if not self.year:
+            error_message += "Please enter a year.\n"
+        
+        if error_message:
+            messagebox.showerror("Error", error_message)
+        else:
+            if not re.match(r"[^@]+@[^@]+\.[^@]+", self.email):
+                messagebox.showerror("Error", "Please enter a valid email address")
+            elif not re.match(r"^[0-9]{10}$", self.mobile):
+                messagebox.showerror("Error", "Please enter a valid mobile number (10 digits)")
+            else:
+                try:
+                    cursor.execute("""UPDATE users SET password = "{}", full_name = "{}", email = "{}", mobile = "{}", gender = "{}", role = "{}" WHERE username = "{}";""".format(self.password, self.name, self.email, self.mobile, self.gender, self.role, self.username))
+                    cnx.commit()
+                except Exception as error:
+                    messagebox.showerror("Error", "Something went wrong while updating.\nCheck traceback in terminal.")  
+                    raise Exception("Error", str(error))
+                else:
+                    messagebox.showinfo("Success", "User details updated successfully.")
+                    UsersSection(self.the_frame)
+                    
+        
+        
+        
+        
+        
+      
+        
+        
+        

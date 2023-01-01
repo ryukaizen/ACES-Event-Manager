80bf862459847be14f9df0fe8396e353a3974905
Ryukaizen <ryukaizen@protonmail.com>
2023-01-01 19:49:31 +0530

    enable guest view

--- app.py
+++ app.py
@@ -158,9 +158,7 @@ class AEM(customtkinter.CTk):
                         width=120, 
                         height=42, 
                         corner_radius=10, 
-                        command=lambda: [widget.destroy() for widget in self.winfo_children()] + [AdminDashboard(self)]
-                        # command=lambda: [widget.destroy() for widget in self.winfo_children()] + [GuestDashboard(self)]
-                        
+                        command=lambda: [widget.destroy() for widget in self.winfo_children()] + [GuestDashboard(self)]
                         )
         self.guest_button.place(x=210, y=260)   
         

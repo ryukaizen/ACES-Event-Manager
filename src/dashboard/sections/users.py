98dd573016e7bdf3ee737613b3fdedda3e7e2b33
Ryukaizen <ryukaizen@protonmail.com>
2023-01-01 19:44:36 +0530

    remove user and export data by admin

--- src/dashboard/sections/users.py
+++ src/dashboard/sections/users.py
@@ -1,9 +1,11 @@
 import customtkinter
+import csv
 import mysql.connector
+import openpyxl
 import re
 import time
 
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
 from ...database.db_connect import cursor, cnx
 
 customtkinter.set_appearance_mode("light") 
@@ -55,7 +57,7 @@ class UsersSection:
                         border_width=3,
                         border_color=("#EDF6FA", "#1B1B24"),
                         corner_radius=15,
-                        # command= lambda: UpdateEvent(frame, treeview)
+                        command= lambda: RemoveUser(frame, treeview)
                         )
         remove_user_button.grid(row=3, column=1, sticky="n", padx=35, pady=5)
         
@@ -70,7 +72,7 @@ class UsersSection:
                         border_width=3,
                         border_color=("#EDF6FA", "#1B1B24"),
                         corner_radius=15,
-                        # command= lambda: ExportData(frame)
+                        command= lambda: ExportData(frame)
                         )
         export_data_button.grid(row=4, column=1, sticky="s", padx=35, pady=5)
         
@@ -739,13 +741,180 @@ class UpdateDetails:
                 else:
                     messagebox.showinfo("Success", "User details updated successfully.")
                     UsersSection(self.the_frame)
-                    
+
+class RemoveUser:
+    def __init__(self, frame, treeview):
+        self.the_frame = frame
+        try:
+            selected_item = treeview.selection()[0]
+            self.username = treeview.item(selected_item)['values'][1]
+            self.name = treeview.item(selected_item)['values'][2]
+            self.email = treeview.item(selected_item)['values'][3]
+            self.mobile = treeview.item(selected_item)['values'][4]
+            self.year = treeview.item(selected_item)['values'][5]
+            self.gender = treeview.item(selected_item)['values'][6]
+            self.role = treeview.item(selected_item)['values'][7]
+               
+        except IndexError:
+            messagebox.showerror("Error", "Please select a user to remove.")
+        else:
+            self.dialogue_window = customtkinter.CTkToplevel(fg_color=("#EDF6FA", "#1B1B24"))
+            self.dialogue_window.geometry("500x320")
+            self.dialogue_window.title("Remove this user?")
+            self.dialogue_window.resizable(False, False)
+            
+            question_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Are you sure you want to remove this user?",
+                            font=customtkinter.CTkFont(size=20, weight="bold"),
+                            )
+            question_label.place(x=250, y=40, anchor="center")
+            
+            username_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Username: " + str(self.username),
+                            font=customtkinter.CTkFont(size=16),
+                            )
+            username_label.place(x=130, y=80, anchor="nw")
+            
+            fullname_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Name: " + f"{self.name}",
+                            font=customtkinter.CTkFont(size=16),
+                            )
+            fullname_label.place(x=130, y=120, anchor="nw")
+            
+            email_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Email ID: " + self.email,
+                            font=customtkinter.CTkFont(size=16),
+                            )
+            email_label.place(x=130, y=160, anchor="nw")
+            
+            yes_button = customtkinter.CTkButton(
+                            self.dialogue_window,
+                            text="Yes (Remove User)",
+                            width=160,
+                            height=60,
+                            font=customtkinter.CTkFont(size=16),
+                            fg_color="#FF0000",
+                            hover_color="#7D0000",
+                            border_width=2,
+                            border_color=("#1B1B24","#EDF6FA"),
+                            corner_radius=15,
+                            command=self.remove_user_from_database
+                            )
+            yes_button.place(x=145, y=240, anchor="center")
+            
+            no_button = customtkinter.CTkButton(
+                            self.dialogue_window,
+                            text="No (Go Back)",
+                            width=160,
+                            height=60,
+                            font=customtkinter.CTkFont(size=16),
+                            fg_color="#0065D9",
+                            border_width=2,
+                            border_color=("#1B1B24","#EDF6FA"),
+                            corner_radius=15,
+                            command=self.dialogue_window.destroy
+                            )
+            no_button.place(x=370, y=240, anchor="center")
+    
+    def remove_user_from_database(self):
+        try:
+            cursor.execute("""DELETE FROM users WHERE username = "{}";""".format(self.username))
+            cnx.commit()                         
+        except Exception as error:
+            messagebox.showerror(error.message)
+            raise Exception("Error", str(error))  
+        else:
+            self.dialogue_window.destroy()
+            UsersSection(self.the_frame)
+
+class ExportData:
+    def __init__(self, frame):
+        self.the_frame = frame
+        self.dialogue_window = customtkinter.CTkToplevel(fg_color=("#EDF6FA", "#1B1B24"))
+        self.dialogue_window.geometry("500x400")
+        self.dialogue_window.title("Export Data")
+        self.dialogue_window.resizable(False, False)
         
+        question_label = customtkinter.CTkLabel(
+                        self.dialogue_window,
+                        text="Select the format in which you want to export the data.",
+                        font=customtkinter.CTkFont(size=16),
+                        )
+        question_label.place(x=250, y=80, anchor="center")
         
+        csv_button = customtkinter.CTkButton(
+                        self.dialogue_window,
+                        text=".CSV (Comma Separated Values)",
+                        width=200,
+                        height=60,
+                        font=customtkinter.CTkFont(size=16),
+                        fg_color="#0065D9",
+                        hover_color="#19941B", 
+                        border_width=2,
+                        border_color=("#1B1B24","#EDF6FA"),
+                        corner_radius=15,
+                        command=self.export_data_csv
+                        )
+        csv_button.place(x=250, y=160, anchor="center")
         
-        
-        
-      
-        
-        
-        
+        xlsx_button = customtkinter.CTkButton(
+                        self.dialogue_window,
+                        text=".XLSX (Excel Spreadsheet)",
+                        width=200,
+                        height=60,
+                        font=customtkinter.CTkFont(size=16),
+                        fg_color="#0065D9",
+                        hover_color="#19941B",  
+                        border_width=2,
+                        border_color=("#1B1B24","#EDF6FA"),
+                        corner_radius=15,
+                        command=self.export_data_xlsx
+                        )
+        xlsx_button.place(x=250, y=240, anchor="center")   
+    
+    def export_data_csv(self):
+        self.dialogue_window.destroy()
+        filepath = filedialog.asksaveasfilename(defaultextension='.csv', filetypes=[('CSV files', '*.csv'), ('All files', '*')]) 
+        try:   
+            cursor.execute("""SELECT username, full_name, email, mobile, year, gender, role FROM users;""")
+            data = cursor.fetchall()
+        except Exception as error:
+            raise Exception("Error", error)  
+        else:
+            if filepath:
+                with open(filepath, "w", newline="") as file:
+                    writer = csv.writer(file)
+                    writer.writerow(["Sr. No.", "PRN", "Name", "Email", "Mobile", "Year", "Gender", "Role"])
+                    i = 0
+                    for row in data:
+                        i = i + 1
+                        data = [i, row[0], row[1], row[2], row[3], row[4], row[5], row[6]]
+                        writer.writerow(data)      
+        finally:        
+            UsersSection(self.the_frame)  
+            
+    def export_data_xlsx(self):
+        self.dialogue_window.destroy()
+        filepath = filedialog.asksaveasfilename(defaultextension='.xlsx', filetypes=[('Excel files', '*.xlsx'), ('All files', '*')])
+        try:
+            cursor.execute("""SELECT username, full_name, email, mobile, year, gender, role FROM users;""")
+            data = cursor.fetchall()
+        except Exception as error:
+            raise Exception("Error", error)
+        else:
+            if filepath:
+                workbook = openpyxl.Workbook()
+                sheet = workbook.active
+                sheet.append(["Sr. No.", "PRN", "Name", "Email", "Mobile", "Year", "Gender", "Role"])
+                i = 0
+                for row in data:
+                    i= i + 1
+                    newrow = [i, str(row[0]), row[1], row[2], str(row[3]), row[4], row[5], row[6]]
+                    sheet.append(newrow)
+                workbook.save(filepath)
+        finally:        
+            UsersSection(self.the_frame)  

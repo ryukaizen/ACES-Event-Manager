33642da2a4a31a1f9fe20050b3e05242734b6a3d
Ryukaizen <ryukaizen@protonmail.com>
2023-01-01 16:22:13 +0530

    WhatsApp addon using pywhatkit

--- src/dashboard/sections/broadcast.py
+++ src/dashboard/sections/broadcast.py
@@ -1,6 +1,7 @@
 import customtkinter
 import mysql.connector
 import os
+import pywhatkit
 import re
 import time
 import smtplib
@@ -21,7 +22,7 @@ EMAIL_SERVER_HOST = os.getenv('EMAIL_SERVER_HOST')
 EMAIL_SERVER_PORT = os.getenv('EMAIL_SERVER_PORT') 
 
 # Make sure to give your bot admin permissions.
-CHANNELS = os.getenv('CHANNELS') 
+TELEGRAM_CHANNEL = os.getenv('TELEGRAM_CHANNEL') 
 
 customtkinter.set_appearance_mode("light") 
 customtkinter.set_default_color_theme("dark-blue")
@@ -68,7 +69,7 @@ class BroadcastSection:
                         width=180, 
                         height=65, 
                         corner_radius=30,
-                        # command= lambda: UpdateEvent(frame, treeview)
+                        command= lambda: PublishWhatsApp(frame, treeview)
                         )
         publish_whatsapp_button.place(relx=0.7, rely=0.1, anchor="n")
     
@@ -1270,7 +1271,7 @@ class PublishTelegram:
             self.note_label.place(relx=0.5, rely=0.8, anchor="n")
     
     def publish(self):
-        chat_id = CHANNELS
+        chat_id = TELEGRAM_CHANNEL
         try:
             button_markup = {'inline_keyboard': [[{'text': '📸 Instagram', 'url': 'https://www.instagram.com/aces_dbatu'}, {'text': '🌐 Visit', 'url': 'https://www.cse.dbatu.ac.in/?page_id=2458'}]]}
             if self.attachment_filepath != None and self.socialmedia_checkbox.get() == "on":
@@ -1287,6 +1288,328 @@ class PublishTelegram:
             self.dialogue_window.destroy()
             messagebox.showerror("Error", str(error))
             self.prompt_test_publish()
+        else:
+            self.dialogue_window.destroy()
+            messagebox.showinfo("Success", "Message sent successfully!")
+
+class PublishWhatsApp:
+    def __init__(self, frame, treeview):
+        self.the_frame = frame
+        try:
+            selected_item = treeview.selection()[0]
+            self.event_name = treeview.item(selected_item)['values'][1]
+            self.event_description = treeview.item(selected_item)['values'][2]
+            self.event_date = treeview.item(selected_item)['values'][3]
+            self.event_time = treeview.item(selected_item)['values'][4]
+            self.event_venue = treeview.item(selected_item)['values'][5]
+            self.event_id = treeview.item(selected_item)['values'][6]
+        
+        except IndexError:
+            messagebox.showerror("Error", "Please select an event to publish using WhatsApp.")
+        else:
+            for widget in frame.winfo_children():
+                widget.destroy()
+            self.publish_whatsapp(frame) 
+    
+    def publish_whatsapp(self, frame): 
+            
+        go_back_button = customtkinter.CTkButton(
+                        frame,
+                        text="Go Back",
+                        font=customtkinter.CTkFont(size=16),
+                        fg_color="Green",
+                        hover_color="#2AAAFA",
+                        border_width=3,
+                        border_color=("#EDF6FA", "#1B1B24"),
+                        corner_radius=15,
+                        command=lambda: BroadcastSection(frame)
+                        )
+        go_back_button.place(relx=0.08, rely=0.02, anchor="n") 
+        
+        configure_content_label = customtkinter.CTkLabel(
+                        frame,
+                        text='Configure the WhatsApp Message',
+                        font=customtkinter.CTkFont(size=26, weight="bold"),
+                        )
+        configure_content_label.place(relx=0.5, rely=0.02, anchor="n")
+        
+        event_name_label = customtkinter.CTkLabel(
+                        frame,
+                        text='Name:',
+                        font=customtkinter.CTkFont(size=18), 
+                        )
+        event_name_label.place(x=80, y=80, anchor="center")
+        
+        self.event_name_entry = customtkinter.CTkEntry(
+                        frame,
+                        width=330,
+                        height=25,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=3,
+                        border_color=("#1B1B24", "#EDF6FA"),
+                        corner_radius=10,
+                        )
+        self.event_name_entry.place(x=280, y=80, anchor="center")
+        
+        event_description_label = customtkinter.CTkLabel(
+                        frame,
+                        text='Description:',
+                        font=customtkinter.CTkFont(size=18), 
+                        )
+        event_description_label.place(x=100, y=120, anchor="center")
+            
+        self.event_description_entry = customtkinter.CTkTextbox(
+                        frame,
+                        text_color=("#1B1B24", "#EDF6FA"),
+                        width=400,
+                        height=200,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=3,
+                        border_color=("#1B1B24", "#EDF6FA"),
+                        corner_radius=10,
+                        )
+        self.event_description_entry.place(x=250, y=240, anchor="center")
+        
+        event_date_label = customtkinter.CTkLabel(
+                        frame,
+                        text='Date:',
+                        font=customtkinter.CTkFont(size=18), 
+                        )
+        event_date_label.place(x=80, y=380, anchor="center")
+        
+        self.event_date_entry = customtkinter.CTkEntry(
+                        frame,
+                        width=100,
+                        height=25,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=3,
+                        border_color=("#1B1B24", "#EDF6FA"),
+                        corner_radius=10,
+                        )
+        self.event_date_entry.place(x=100, y=420, anchor="center")
+        
+        event_time_label = customtkinter.CTkLabel(
+                        frame,
+                        text="Time:",
+                        font=customtkinter.CTkFont(size=18), 
+                        )
+        event_time_label.place(x=200, y=380, anchor="center")
+        
+        self.event_time_entry = customtkinter.CTkEntry(
+                        frame,
+                        width=100,
+                        height=25,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=3,
+                        border_color=("#1B1B24", "#EDF6FA"),
+                        corner_radius=10,
+                        )
+        self.event_time_entry.place(x=220, y=420, anchor="center")
+        
+        event_venue_label = customtkinter.CTkLabel(
+                        frame,
+                        text="Venue:",
+                        font=customtkinter.CTkFont(size=18), 
+                        )
+        event_venue_label.place(x=320, y=380, anchor="center")
+        
+        self.event_venue_entry = customtkinter.CTkEntry(
+                        frame,
+                        width=160,
+                        height=25,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=3,
+                        border_color=("#1B1B24", "#EDF6FA"),
+                        corner_radius=10,
+                        )
+        self.event_venue_entry.place(x=370, y=420, anchor="center")
+        
+        generate_content_button = customtkinter.CTkButton(
+                            frame,
+                            text="Generate Content",
+                            font=customtkinter.CTkFont(size=18), 
+                            fg_color="#0065D9",
+                            hover_color="#19941B",  
+                            width=180, 
+                            height=65, 
+                            border_width=3,
+                            border_color=("#EDF6FA", "#1B1B24"),
+                            corner_radius=15,
+                            command=self.generate_whatsapp_text 
+                            )
+        generate_content_button.place(x=660, y=410, anchor="center")
+        
+        save_content_button = customtkinter.CTkButton(
+                            frame,
+                            text="Save Content",
+                            font=customtkinter.CTkFont(size=18), 
+                            fg_color="#0065D9",
+                            hover_color="#19941B",  
+                            width=180, 
+                            height=65, 
+                            border_width=3,
+                            border_color=("#EDF6FA", "#1B1B24"),
+                            corner_radius=15,
+                            command=self.save_generated_content
+                            )
+        save_content_button.place(x=900, y=410, anchor="center")
+        
+        self.generated_content = customtkinter.CTkTextbox(
+                        frame,
+                        text_color=("#1B1B24", "#EDF6FA"),
+                        width=550,
+                        height=290,
+                        font=customtkinter.CTkFont(size=18),
+                        border_width=3,
+                        border_color=("#1B1B24", "#EDF6FA"),
+                        corner_radius=10,
+                        )
+        self.generated_content.place(x=780, y=220, anchor="center")
+        
+        publish_button = customtkinter.CTkButton(
+                            frame,
+                            text="Publish",
+                            font=customtkinter.CTkFont(size=20, weight="bold"), 
+                            fg_color="#D96C00",
+                            hover_color="#FF8C00",  
+                            width=180, 
+                            height=65, 
+                            border_width=3,
+                            border_color=("#EDF6FA", "#1B1B24"),
+                            corner_radius=15,
+                            command=self.prompt_publish
+                            )
+        publish_button.place(relx=0.88, rely=0.35, anchor="n") 
+        
+        ################################ Insert existing values ################################
+        
+        self.event_name_entry.insert(0, self.event_name)
+        self.event_description_entry.insert("0.0", self.event_description.strip())
+        self.event_date_entry.insert(0, self.event_date)
+        self.event_time_entry.insert(0, self.event_time)
+        self.event_venue_entry.insert(0, self.event_venue.strip())
+        
+        # This inserts already generated content in the textbox, if any exists
+        try:
+            query = """SELECT content FROM broadcast WHERE event_id = "{}";""".format(self.event_id)
+            cursor.execute(query)
+            content = cursor.fetchone()[0]
+        except Exception:
+            messagebox.showinfo("Configure WhatsApp Message", "Found no saved content for event_id: {}\n\nKindly generate content first, edit it by your own according and then save it for later use.".format(self.event_id))
+        else:
+            if content:
+                self.generated_content.insert("0.0", content)
+                
+    def generate_whatsapp_text(self):
+        event_name = self.event_name_entry.get()
+        event_description = self.event_description_entry.get("0.0", "end-1c")
+        event_date = self.event_date_entry.get()
+        event_time = self.event_time_entry.get()
+        event_venue = self.event_venue_entry.get()
+        
+        # Remove extra empty lines from the end
+        event_name = event_name.strip()
+        event_description = event_description.strip()
+        event_venue = event_venue.strip()
+        
+        telegram_text = f"""Hey there,\n\n{event_name} is coming up soon!\n\n{event_description}\n\nDate: {event_date}\nTime: {event_time}\nVenue: {event_venue}\n\nPS: Participate in large numbers!\n\nRegards,\nTeam ACES"""
+        
+        self.generated_content.delete("0.0", "end")
+        self.generated_content.insert("0.0", telegram_text)
+    
+    def save_generated_content(self):
+        final_content = self.generated_content.get("0.0", "end-1c")
+        if len(final_content) == 0:
+            messagebox.showerror("Error", "Content is empty!")
+            return 
+        
+        # Remove extra empty lines from the end
+        final_final_content = final_content.strip()
+        
+        # Format it as per MySQL entry
+        escaped_final_content = mysql.connector.conversion.MySQLConverter().escape(final_final_content) 
+        self.final_final_final_content = escaped_final_content
+               
+        try:
+            query = """INSERT INTO broadcast (event_id, content) VALUES ("{}", "{}") ON DUPLICATE KEY UPDATE content = "{}";""".format(self.event_id,  self.final_final_final_content,  self.final_final_final_content)
+            cursor.execute(query)
+            cnx.commit() 
+        except Exception as error:
+            raise Exception("Error", str(error))  
+        else:
+            messagebox.showinfo("Success", "Content saved successfully!")
+    
+    def prompt_publish(self):
+        self.body = self.generated_content.get("0.0", "end-1c")
+        
+        if len(self.body) == 0:
+            messagebox.showerror("Error", "Content is empty!")
+            return
+        else:
+            self.dialogue_window = customtkinter.CTkToplevel(fg_color=("#EDF6FA", "#1B1B24"))
+            self.dialogue_window.geometry("700x300")
+            self.dialogue_window.title("Publish WhatsApp")
+            self.dialogue_window.resizable(False, False)
+            
+            question_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text=f"This will send a WhatsApp on your registered mobile no.\n\nYou will be redirected to WhatsApp web.\nMake sure you are logged in to WhatsApp (web.whatsapp.com)",
+                            font=customtkinter.CTkFont(size=20, weight="bold", slant="roman")
+                            )
+            question_label.place(relx=0.5, rely=0.1, anchor="n")
+        
+            publish_button = customtkinter.CTkButton(
+                            self.dialogue_window,
+                            text="Publish",
+                            font=customtkinter.CTkFont(size=20, weight="bold"), 
+                            fg_color="#D96C00",
+                            hover_color="#FF8C00", 
+                            width=180, 
+                            height=65, 
+                            border_width=3,
+                            border_color=("#EDF6FA", "#1B1B24"),
+                            corner_radius=15,
+                            command=self.publish
+                            )
+            publish_button.place(relx=0.3, rely=0.55, anchor="n")
+        
+            cancel_button = customtkinter.CTkButton(
+                            self.dialogue_window,
+                            text="Cancel",
+                            font=customtkinter.CTkFont(size=20, weight="bold"), 
+                            fg_color="#0065D9",
+                            hover_color="#FF0000",  
+                            width=180, 
+                            height=65, 
+                            border_width=3,
+                            border_color=("#EDF6FA", "#1B1B24"),
+                            corner_radius=15,
+                            command=lambda: self.dialogue_window.destroy()
+                            )
+            cancel_button.place(relx=0.7, rely=0.55, anchor="n")
+            
+            self.note_label = customtkinter.CTkLabel(
+                            self.dialogue_window,
+                            text="Note: Do not close this window until the message is posted.",
+                            font=customtkinter.CTkFont(size=16, slant="roman"),
+                            width=50,
+                            height=25,
+                            corner_radius=0
+                            )
+            self.note_label.place(relx=0.5, rely=0.8, anchor="n")
+    
+    def publish(self):
+        f = open('src/dashboard/sections/session.txt', 'r')
+        username = f.read()
+        f.close()
+        try:
+            cursor.execute("""SELECT mobile FROM users WHERE username = "{}";""".format(username))
+            mobile = cursor.fetchone()[0]
+            pywhatkit.sendwhatmsg_instantly(f"+91{mobile}", self.body)
+        except Exception as error:
+            self.dialogue_window.destroy()
+            messagebox.showerror("Error", str(error))
+            self.prompt_publish()
         else:
             self.dialogue_window.destroy()
             messagebox.showinfo("Success", "Message sent successfully!")
